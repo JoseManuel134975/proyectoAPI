6e5705f806ef38bd6ec6dc35dd1e4c8979b3ea92
José Manuel <josemanuel3606@gmail.com>
2024-12-24 23:54:53 +0100

    Testing Bundles of API...

--- src/js/products.js
+++ src/js/products.js
@@ -7,17 +7,43 @@ const getData = async () => {
 
 
     shopJSON.data.entries.forEach(obj => {
-        if("bundle" in obj) {
-                const img = document.createElement('img')
-                if("image" in obj.bundle) {
-                        img.src = obj.bundle.image
-                        
-                }
+        if ("bundle" in obj) {
+            const sectionCards = document.querySelector('.cards')
 
-            
-            document.body.appendChild(img)
+            const card = document.createElement('article')
+            card.className = 'card'
+
+            const img = document.createElement('img')
+            const name = document.createElement('h2')
+            const regularPrice = document.createElement('p')
+            const category = document.createElement('h3')
+
+            if ("image" in obj.bundle) {
+                img.src = obj.bundle.image
+            }
+
+            if ("name" in obj.bundle) {
+                name.textContent = obj.bundle.name
+            }
+
+            if ("regularPrice" in obj) {
+                regularPrice.textContent = `${obj.regularPrice} V-Bucks`
+            }
+
+            if ("layout" in obj) {
+                if ("category" in obj.layout) {
+                    category.textContent = obj.layout.category
+                }
+            }
+
+            card.appendChild(img)
+            card.appendChild(name)
+            card.appendChild(regularPrice)
+            card.appendChild(category)
+
+            sectionCards.appendChild(card)
         }
     });
-} 
+}
 
 document.addEventListener("DOMContentLoaded", getData);
